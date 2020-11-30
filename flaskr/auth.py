88bf0e5295ce08451dfd827f46a817e7b81861bb
LSWarss <ls.warss98@gmail.com>
2020-11-30 18:23:37 +0100

    Last corrections after testing.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -6,9 +6,9 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 """
-A Blueprint is a way to organize a group of related views and other code. 
-Rather than registering views and other code directly with an application, 
-they are registered with a blueprint. Then the blueprint is registered 
+A Blueprint is a way to organize a group of related views and other code.
+Rather than registering views and other code directly with an application,
+they are registered with a blueprint. Then the blueprint is registered
 with the application when it is available in the factory function.
 """
 
@@ -23,14 +23,14 @@ def register():
         error = None
 
         if not username:
-            error = 'Username is required'
+            error = 'Username is required.'
         elif not password:
-            error = 'Passowrd is required'
+            error = 'Password is required.'
         elif db.execute(
             'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
             error = f'User {username} is already registered.'
-        
+
         if error is None:
             db.execute(
                 'INSERT INTO user (username, password) VALUES (?, ?)', (username, generate_password_hash(password),)
@@ -53,11 +53,11 @@ def login():
             'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
-        if user is None: 
+        if user is None:
             error = 'Incorrect username'
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password'
-        
+
         if error is None:
             session.clear()
             session['user_id'] = user['id']
