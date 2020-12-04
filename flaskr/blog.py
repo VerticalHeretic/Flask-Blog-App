4475b3fb5568b596fc3014573294dc4957574d4b
Lukasz Stachnik <lukasz.stachnik@ing.pl>
2020-12-04 23:33:51 +0100

    Added like icon for the posts.And tweaked database schema to implement likes.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -46,7 +46,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id,likeCount,username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -59,6 +59,7 @@ def get_post(id, check_author=True):
         abort(403)
 
     return post
+
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
---
+++ flaskr/static/utility.js
@@ -0,0 +1,9 @@
+function like(x) {
+    if(x.style.color != "red"){
+        x.style.color = "red";
+    }
+    else{
+        x.style.color = "black";
+    }
+  }
+
